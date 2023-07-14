9b17b4c6ebd34e89a62e6d9697714571f03536f3
lindhage22 <132088006+lindhage22@users.noreply.github.com>
2023-07-13 23:37:21 -0600

    Add files via upload

--- ProjectsApp.java
+++ ProjectsApp.java
@@ -14,11 +14,15 @@ import projects.service.ProjectService;
 public class ProjectsApp {
 
 	private Scanner scanner = new Scanner(System.in);
-	private ProjectService projectService = new ProjectService(); 
+	private ProjectService projectService = new ProjectService();
+	private Project curProject;
+	
 	
 	//@formatter:off
 	private List<String> operations =List.of(
-			"1) Add a project"
+			"1) Add a project",
+			"2) List projects",
+			"3) Select a project"
 		);
 	//@formatter:off
 	public static void main(String[]args) {
@@ -39,6 +43,12 @@ public class ProjectsApp {
 		case 1: 
 			createProject();
 			break;
+		case 2:
+			listProjects();
+			break;
+		case 3:
+			selectProjects();
+			break;
 			
 	 default:
 		 System.out.println("\n" + selection + " is not a valid selection. Try again.");
@@ -51,6 +61,32 @@ public class ProjectsApp {
 	 }
 	}	
    }
+	private void selectProjects() {
+		 listProjects();
+	     Integer projectId = getIntInput("Enter a project ID to select a project");
+
+	     
+	     /* Unselect the current project. */
+	     
+	     curProject = null;
+	     
+	     /*This will throw an exception if an invalid project ID is entered. */
+	     
+	     curProject = projectService.fetchProjectById(projectId);
+		
+	}
+
+	private void listProjects() {
+		List<Project> projects = projectService.fetchAllProjects();
+		
+		System.out.println("\nProjects:");
+		
+		projects.forEach(project ->System.out.println("  " + project.getProjectId()
+		   + ": " + project.getProjectName()));
+		
+		/*Not sure why in the instructions it ask to do with out " " but in the solution it says too*/
+	}
+
 	private void createProject(){
 		String projectName = getStringInput("Enter the project name");
 		BigDecimal estimatedHours = getDecimalInput ("Enter the estimated hours");
@@ -124,10 +160,24 @@ public class ProjectsApp {
 	/* This is the print menu selections, one per line. */
 
 	private void printOperations() {
-		System.out.println("\n These are the available selections. Press the enter key to quit:");
+		System.out.println("\n These are the available selections."
+				+ "Press the enter key to quit:");
 		
 		/* Lambda Expression*/
 		operations.forEach(line -> System.out.println(" " + line));
-	}
-}	
+		
+		/*With enhanced for loop*/
+		//for(Sting line : operations){
+		//System.out.println( " " + line);
+		//}
+
+	if (Objects.isNull(curProject)){
+        System.out.println("\nYou are not working with a project. ");
+    }
+    else {
+        System.out.println("\nYou are working with this project: " + curProject);
+    }
+}
+}
+
 		
